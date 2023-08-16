7dd5477f167fc2affdb4cd7d6c5c107359f85b5e
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-08-16 17:51:07 +0300

    Initial Commit

---
+++ fw-parking-lot-pitch/fw_parking_lot_pitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fw_parking_lot_pitchApp.swift
+//  fw-parking-lot-pitch
+//
+//  Created by Taha Chaudhry on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct fw_parking_lot_pitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
